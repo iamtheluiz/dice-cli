0de46a283a1705dcaff9d04ec4dbec430ee01c8c
iamtheluiz <iamtheluiz.dev@gmail.com>
2021-07-02 23:46:08 -0300

    feat: add express lib

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
-class Program {
-  static run(): void {
-    console.log('Hello');
-    console.log('What do you want?');
-  }
-}
+import express from 'express';
 
-Program.run();
+const app = express();
+const routes = express.Router();
+const port = 3000;
+
+routes.get('/', (request, response) => {
+  const info = {
+    name: 'Dice Roll API',
+    repository: 'https://github.com/iamtheluiz/dice-roll',
+  };
+
+  return response.json(info).status(200);
+});
+
+app.use(routes);
+
+app.listen(port, () => {
+  console.log('Server running at http://localhost:3000/');
+});
