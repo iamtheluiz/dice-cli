32b51f8dd54cd07d1000da038e580c6d839491ce
iamtheluiz <iamtheluiz.dev@gmail.com>
2021-07-06 00:49:22 -0300

    feat: add handlebars

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import expressHandlebars from 'express-handlebars';
+import path from 'path';
 
 import routes from './routes';
 
@@ -8,6 +10,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Handlebars
+app.set('views', path.join(__dirname, 'views'));
+app.engine(
+  'hbs',
+  expressHandlebars({
+    defaultLayout: 'template',
+    extname: '.hbs',
+  }),
+);
+app.set('view engine', 'hbs');
+
 app.use(routes);
+app.use(express.static('public'));
 
 export default app;
